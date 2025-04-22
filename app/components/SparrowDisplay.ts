80ff5513c96370355decd0349f02d7aa4a40d270
yjgaia <yjdev88@gmail.com>
2025-04-22 21:03:24 +0900

    Refactor SparrowDisplay constructor to use SparrowDisplayData interface for improved type safety

--- app/components/SparrowDisplay.ts
+++ app/components/SparrowDisplay.ts
@@ -1,10 +1,12 @@
 import { DomNode } from "@commonmodule/app";
 
+interface SparrowDisplayData {
+  style: "Illustration" | "Pixel Art";
+  parts: { [partName: string]: string };
+}
+
 export default class SparrowDisplay extends DomNode {
-  constructor(data: {
-    style: "Illustration" | "Pixel Art";
-    parts: { [partName: string]: string };
-  }) {
+  constructor(data: SparrowDisplayData) {
     super(".sparrow-display");
   }
 }
